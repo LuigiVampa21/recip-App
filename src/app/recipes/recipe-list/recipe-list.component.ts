1fc5189549d4cdeb67b8fd9b0847aab64e4d6b81
TEISSIER Nathan <luigivampa12@protonmail.ch>
2022-09-21 11:27:49 +0200

    ntn

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Recipe } from '../recipe.model';
 
 @Component({
   selector: 'app-recipe-list',
@@ -6,10 +7,22 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./recipe-list.component.scss']
 })
 export class RecipeListComponent implements OnInit {
-
+  recipes: Recipe[] = [
+    new Recipe('The Test Recipe', 'This is a test', 'https://pixabay.com/fr/photos/pizza-tranches-cuisine-italienne-329523/')
+  ];
+  recipeName: string = '';
+  recipeDescription: string = '';
+  recipeImagePath: string = '';
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  onShowRecipe(){
+    const recipe = this.recipes[0];
+    this.recipeName = recipe.name;
+    this.recipeDescription = recipe.description;
+    this.recipeImagePath = recipe.imagepath;
   }
 
 }
